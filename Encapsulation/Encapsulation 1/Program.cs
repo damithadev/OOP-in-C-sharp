2821c75ee34413da82a0840b5290033167e28849
Damitha <hellodamitha@gmail.com>
2023-04-09 15:42:23 +0530

    Add project files.

---
+++ Encapsulation/Encapsulation 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object 1
+            person person1 = new person();
+
+            //creating object 2
+            person person2 = new person();
+
+            //Accessing properties of the class
+            person1.name = "Damitha";
+            person1.age = 20;
+
+            person2.name = "Jayatharaka";
+            person2.age = 21;
+
+
+            //printing properties of the class
+            Console.WriteLine("Name is: " +person1.name);
+            Console.WriteLine("Age is: " + person1.age);
+
+            //Accessing methods of the class
+            person2.eating();
+            person2.sleeping();
+
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation 1/person.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_1
+{
+    internal class person
+    {
+        public string name;
+        public int age;
+
+
+
+        /*
+        -----------access mutator - short method---------
+       
+        public string name {  get; set; }
+        public int age { get; set; }
+        */
+
+
+
+
+        /*
+        -----------access mutator - long method----------
+
+        //properties
+        private string name;
+        private int age;
+        
+        //access mutating
+        public string Name
+        { 
+            get                    //read
+            { 
+                return name; 
+            }
+            set                    //write
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get                     //read
+            {
+                return age;
+            }
+            set                     //write
+            {
+                age = value;
+            }
+        }
+        */
+
+
+        /*
+        //create read only property using long access mutator
+        public int Age
+        {
+            get                     //read
+            {
+                return age;
+            }
+            private set                     //write disabled
+            {
+                
+            }
+        }
+
+        */
+
+
+        //methods
+        public void eating()
+        {
+            Console.WriteLine("I'm eating");
+
+        }
+
+        public void sleeping () 
+        {
+            Console.WriteLine("I'm sleeping");
+        }
+
+
+    }
+}
